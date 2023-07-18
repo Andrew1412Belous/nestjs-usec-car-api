1f4da76c5a8079836b551cfee12cf575064f48a5
Andrii <andrew.belous1412@gmail.com>
2023-07-18 12:16:05 +0300

    add routes & configure users service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Query, Delete } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -9,5 +10,25 @@ export class UsersController {
 	@Post('/signup')
 	createUser(@Body() body: CreateUserDto) {
 		this.usersService.create(body.email, body.password);
+	}
+
+	@Get('/:id')
+	findUser(@Param('id') id: string) {
+		return this.usersService.findOne(parseInt(id));
+	}
+
+	@Get()
+	findAllUsers(@Query('email') email: string) {
+		return this.usersService.find(email);
+	}
+
+	@Delete('/:id')
+	removeUser(@Param('id') id: string) {
+		return this.usersService.remove(parseInt(id));
+	}
+
+	@Patch('/:id')
+	updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+		return this.usersService.update(parseInt(id), body);
 	}
 }
