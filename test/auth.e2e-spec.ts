39bfa42b9362eddecbf0de3609864fbc988d9bd6
Andrii <andrew.belous1412@gmail.com>
2023-07-20 12:33:05 +0300

    add auth e2e test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('Authentication System', () => {
 	let app: INestApplication;
 
 	beforeEach(async () => {
@@ -15,8 +15,22 @@ describe('AppController (e2e)', () => {
 		await app.init();
 	});
 
-	it('/ (GET)', () => {
-		return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+	it('handles a signup request', async () => {
+		const user = {
+			email: 'testsome@test.com',
+			password: 'password',
+		};
+
+		return request(app.getHttpServer())
+			.post('/auth/signup')
+			.send(user)
+			.expect(200)
+			.then(({ body }) => {
+				const { id, email } = body;
+
+				expect(id).toBeDefined();
+				expect(email).toEqual(user.email);
+			});
 	});
 
 	afterAll(async () => {
